34ef6d98119465390b8dd1393c9d27d34af9f5ab
Ivan Vivar <ivaanv23@gmail.com>
2026-05-18 00:03:10 +0200

    feat: update

---
+++ ejercicios/calculaestadisticas.py
@@ -0,0 +1,7 @@
+estadisticas = [5, 9, 3, 10, 7, 10]
+
+print ("El total de las estadisticas es: ", sum(estadisticas))
+print ("La cantidad de estadisticas es: ", len(estadisticas))
+print ("El numero mas alto de las estadisticas es: ", max(estadisticas))
+print ("El numero mas bajo de las estadisticas es: ", min(estadisticas))
+print ("La media de las estadisticas es: ", sum(estadisticas)/len(estadisticas))
--- fundamentos/11_funciones.py
+++ fundamentos/11_funciones.py
@@ -20,21 +20,42 @@ hola_nombre("Ivan")
 
 def resta (a, b):
     return a-b
-resta(5, 3)
-print(resta(5, 3))
+print(resta(5, 3)) # Resultado 2
 
 # Argumentos por defecto
 
 def suma (a, b, c=20):
     return a+b+c
-suma (5,5) # Dado que el parámetro c tiene un valor por defecto, la función puede ser llamada sin ese valor.
-
-print (suma (5, 5))
+print (suma (5, 5)) # Dado que el parámetro c tiene un valor por defecto, la función puede ser llamada sin ese valor.
 
 # Asignar valor por defecto a todos los parametros
 
 def sumapred(a=3, b=5, c=0):
     return a+b+c
-sumapred() 
-print (sumapred())
+print (sumapred()) # Resultado 8 
+
+# Argumentos de longitud variable
+
+def sumavar (numeros):
+    total = 0
+    for n in numeros:
+        total += n
+    return total
+print(sumavar([1, 3, 5, 4]))
+
+# Sentencia return 
+# El uso de la sentencia return permite realizar dos cosas:
+# Salir de la función y transferir la ejecución de vuelta a donde se realizó la llamada.
+# Devolver uno o varios parámetros, fruto de la ejecución de la función.
+
+def mi_funcion():
+    print("Entra en mi_funcion")
+    return
+    print("No llega")
+print (mi_funcion()) # Entra en mi_funcion
+
+def saludar(nombre, mensaje="Hola"):
+    print(f"{mensaje}, {nombre}")
+saludar("Ivan", "Buenos días")
+
 
