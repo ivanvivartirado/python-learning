eda21a1811745ba4db8b0c65857eaa8cf497fef4
Ivan Vivar <ivaanv23@gmail.com>
2026-05-17 21:50:59 +0200

    feat: update

---
+++ ejercicios/cadenastexto.py
@@ -0,0 +1,4 @@
+cadenatexto = "Hola mi nombre es Ivan Vivar soy estudiante de ASIR y tengo 19 años estoy aprendiendo Python!"
+print(cadenatexto)
+cadenatexto2 = "Hola mi nombre es Ivan Vivar\n Soy estudiante de ASIR y tengo 19 años estoy aprendiendo Python!"
+print(cadenatexto2)
