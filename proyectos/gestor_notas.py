1c79a8c5ad81655694f8324faeb0879abc19e2c9
Ivan Vivar <ivaanv23@gmail.com>
2026-05-18 20:49:56 +0200

    feat: update

---
+++ proyectos/gestor_notas.py
@@ -0,0 +1,17 @@
+print ("Que quieres hacer?")
+print ("1. Ver notas")
+print ("2. Agregar nota")
+print ("3. Salir")
+
+opcion = input ("Elige una opcion:")
+
+if opcion == "1":
+    with open ("proyectos/agenda.txt", "r") as f:
+        print (f.read())
+elif opcion == "2":
+    with open ("proyectos/agenda.txt", "a") as f:
+        nota = input ("Escribe tu nota: ")
+        f.write(nota + "\n")
+    print ("Nota guardada.")
+elif opcion == "3":
+    print("Hasta luego.")
